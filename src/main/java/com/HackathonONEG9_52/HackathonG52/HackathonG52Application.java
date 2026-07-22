890011ac64fedb37c4be5e4e15996cdb41732e22
Marcos Mazzanti <marcossantinomazzanti@gmail.com>
2026-07-21 22:31:50 -0300

    Creacion del proyecto

---
+++ src/main/java/com/HackathonONEG9_52/HackathonG52/HackathonG52Application.java
@@ -0,0 +1,13 @@
+package com.HackathonONEG9_52.HackathonG52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonG52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonG52Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HackathonONEG9_52/HackathonG52/HackathonG52ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HackathonONEG9_52.HackathonG52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonG52ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
