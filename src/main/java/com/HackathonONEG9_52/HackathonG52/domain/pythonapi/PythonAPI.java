fbae41911760ab2e4569fa686bbea0eb0b119221
Tomanotar <tomi_550@hotmail.es>
2026-07-30 17:35:26 -0300

    Update PythonAPI.java

--- src/main/java/com/HackathonONEG9_52/HackathonG52/domain/pythonapi/PythonAPI.java
+++ src/main/java/com/HackathonONEG9_52/HackathonG52/domain/pythonapi/PythonAPI.java
@@ -19,7 +19,7 @@ public class PythonAPI {
     private final ObjectMapper objectMapper;
 
     public PythonAPI() {
-        this.pythonApiUrl = "http://163.176.132.115:9999/contenido";
+        this.pythonApiUrl = "http://163.176.132.115:8000/contenido";
         // debe ser  8000/contenido para sincronizar con denisse
         this.client = HttpClient.newHttpClient();
         this.objectMapper = new ObjectMapper();
